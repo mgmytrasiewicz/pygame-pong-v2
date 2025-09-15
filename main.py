35e56ea9a3837d04e1bfc1edd190fef5ace6c53c
Michal Gmytrasiewicz <michal.gmytrasiewicz@data.pl>
2025-09-15 19:41:49 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ import pygame
 pygame.init()
 
 # Set up display
+BORDER = 10
 WIDTH, HEIGHT = 800, 600
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(pygame.Color("black"))
+
 pygame.display.set_caption("The Pong Game")
 
+pygame.draw.rect(screen, pygame.Color("white"),\
+                 pygame.Rect(0, 0, WIDTH, BORDER))  # Top border
+pygame.draw.rect(screen, pygame.Color("white"),\
+                    pygame.Rect(0, HEIGHT - BORDER, WIDTH, BORDER))  # Bottom border
+pygame.draw.rect(screen, pygame.Color("white"),\
+                    pygame.Rect(0, 0, BORDER, HEIGHT))  # Left border
+
+pygame.display.flip()
 while True:
     e = pygame.event.poll()
     if e.type == pygame.QUIT:
         break
 
-quit
-                           
+pygame.quit()
